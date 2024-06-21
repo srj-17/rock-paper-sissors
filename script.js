5517cbec924a5bb622db734e75395157c584117c
srj-17 <saugatrijal17@gmail.com>
2024-06-21 17:58:11 +0545

    Do winning game, but a better implementation
    Decrease lag after button press

--- script.js
+++ script.js
@@ -33,57 +33,53 @@ function playRound(humanChoice, computerChoice) {
             humanChoice == 'paper' && computerChoice == 'sissors' ||
             humanChoice == 'sissors' && computerChoice == 'rock') {    
                 computerScore++
-                console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
                 return `You lose! ${computerChoice} beats ${humanChoice}`;
 
         } else {
             humanScore ++;
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
             return `You win! ${humanChoice} beats ${computerChoice}`;
         }
     }
 }
-
-// function playAgain() {
-//     play = prompt("Would you like to play again? [yes(y)/no(n)").toLowerCase();
-//     if (play === 'yes' || play === 'y') {
-//         playGame();
-//     } else {
-//         console.log('Bye!');
-//     }
-// }
 
 // function playGame() {
 //     for (let i = 0; i < 5; i++) {      
 //         playRound(getUserChoice(), getComputerChoice());
 //     }
 
-//     if (humanScore > computerScore) {
-//         console.log("Congratulations! You win the game!");
-//     } else if (humanScore < computerScore) {
-//         console.log("Oh no! Humanity has lost against machines! You lose the game! ");
-//     } else {
-//         console.log("You Draw the game with the computer");
-//     }
+
 
 //     // ask to play again after 3 seconds
-//     const TIMEOUT_LEN = 3 * 1000;
-//     setTimeout(playAgain, TIMEOUT_LEN);
+    
 // }
 
 let choices = document.querySelector('.choices');
+let win = document.querySelector('.win');
+let result = document.querySelector('.result');
 let clickCount = 0;
+
+function printWinner() {
+    if (humanScore > computerScore) {
+        win.textContent = 'Congratulations! You win the game!';
+    } else if (humanScore < computerScore) {
+        win.textContent = 'Alas! Humanity has lost!';
+    } else {
+        win.textContent = 'Here\'s your Draw';
+    }
+    clickCount = 0;
+    const myTimeout = setTimeout(myGreeting, 5000);
+
+}
 
 // add event listener for button press
 choices.addEventListener('click', (event) => {
     clickCount++;
     let target = event.target;
 
-    result = document.querySelector('.result');
     result.textContent = playRound(target.id, getComputerChoice());
+    
     if (clickCount === 5) {
-        win = document.querySelector('.win');
-        win.textContent = 'Congratulations! You win!';
+        printWinner();
     }
 });
 
